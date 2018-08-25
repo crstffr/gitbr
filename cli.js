3538da80c869c5a742d8459cf4a12304c90297ae
Christpher Mason <crstffr@gmail.com>
2018-08-25 16:07:58 -0500

    BranchSelector with branch checkout onSelect

--- cli.js
+++ cli.js
@@ -1,16 +1,2 @@
 #!/usr/bin/env node
-
-const {h, render} = require('ink');
-const jsx = require('import-jsx');
-const Git = require('./lib/services/Git');
-
-let args = process.argv.slice(2);
-
-if (args.length > 0) {
-  Git.call('branch', args);
-  process.exit();
-}
-
-console.log('run gitbr');
-
-// render(h(jsx('./lib/entry')));
+require('./lib/main');
