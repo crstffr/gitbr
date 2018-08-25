9914c5105fbef9ddacdd2f2b5f7d4834f59c823e
Christpher Mason <crstffr@gmail.com>
2018-08-25 10:43:29 -0500

    Removed meow and prop-types, and other unused files

--- cli.js
+++ cli.js
@@ -2,22 +2,9 @@
 
 const importJsx = require('import-jsx');
 const {h, render} = require('ink');
-const meow = require('meow');
 
-const Ui = importJsx('./ui');
+const Entry = importJsx('./lib/entry');
 
-const cli = meow(`
-	Usage
-	  $ gitbr [input]
+console.log(process.argv.slice(2));
 
-	Options
-	  --name  Lorem ipsum [Default: false]
-
-	Examples
-	  $ gitbr
-	  I love Ink
-	  $ gitbr --name=ponies
-	  I love ponies
-`);
-
-render(h(Ui, cli.flags));
+// render(h(Entry));
